730dea2c26cda5973eeb7bcef37a1019e5c7bf71
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-02-22 23:56:46 -0300

    update

--- adapters/controller.go
+++ adapters/controller.go
@@ -11,10 +11,10 @@ func (controller *Controller) AddNewViewer(g *gin.Context) {
 	userAgent := g.Request.UserAgent()
 	viewer := factory.FactoryViewer(userAgent)
 	response := map[string]string{"viewer": viewer.UserAgent()}
-	controller.Response(200, response)
+	controller.Response(200, response, g)
 }
 
-func (controller *Controller) Response(statusCode int, data map[string]string) {
+func (controller *Controller) Response(statusCode int, data map[string]string, g *gin.Context) {
 	response := gin.H{"data": data}
 	g.JSON(statusCode, response)
 }
