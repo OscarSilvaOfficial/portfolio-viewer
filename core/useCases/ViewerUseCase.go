5523a72afb71838814770042d85c0a3e0c1d7b92
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-02-27 13:44:59 -0300

    Criação de um mapper

--- core/useCases/ViewerUseCase.go
+++ core/useCases/ViewerUseCase.go
@@ -5,8 +5,16 @@ import (
 	"github.com/OscarSilvaOfficial/portfolio-viewer/ports"
 )
 
+type RMap struct{
+	value map[string]string
+}
+
 func AddNewViewer(userAgent string, repository ports.ViewerRepository) map[string]string {
 	viewer := domain.FactoryViewer(userAgent)
 	repository.AddNewViewer(viewer)
-	return map[string]string{"viewer": viewer.UserAgent()}
+	return Map("viewer", viewer.UserAgent())
+}
+
+func Map(key string, value string) map[string]string {
+	return map[string]string{key: value}
 }
